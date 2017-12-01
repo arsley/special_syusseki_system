2e2c5b0d55565e995d2312c3feec1063de613a8f
Aoshi_F <19aoshi20@gmail.com>
2017-12-01 13:21:33 +0900

    Delete mailer methods

--- test/mailers/previews/attend_notice_mailer_preview.rb
+++
@@ -1,7 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/attend_notice_mailer
-class AttendNoticeMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/attend_notice_mailer/weekly_summary
-  def weekly_summary
-    AttendNoticeMailer.weekly_summary(Teacher.first)
-  end
-end
